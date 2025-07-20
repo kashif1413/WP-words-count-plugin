7df8c93e811def682852ac02946822fa9cfb0211
Kashif Ajmal <kashifchaudhary1413@gmail.com>
2025-07-20 11:09:25 +0500

    Changes

--- plugin.php
+++ plugin.php
@@ -2,22 +2,18 @@
 /*
 Plugin Name: ToolsMenia's Words Count Tool
 Plugin URI:  https://github.com/kashif1413/WP-words-count-plugin
-Description: A simple case converter tool.
-Version: 1
+Description: A simple words/characters counting tool.
+Version: 1.0
 Author: Kashif Ajmal
 Author URI: https://github.com/kashif1413
 GitHub Plugin URI: kashif1413/WP-words-count-plugin
 Primary Branch: main
 */
 
-
-function cc_tool_shortcode() {
-    ob_start(); ?>
-    <!-- Include your tool HTML/CSS/JS -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@400;700&family=Roboto+Slab:wght@700&display=swap" rel="stylesheet">
-
-    <?php include plugin_dir_path(__FILE__) . 'index.html'; ?>
-    <?php
+// Shortcode output
+function tm_words_count_shortcode() {
+    ob_start();
+    include plugin_dir_path(__FILE__) . 'index.html'; // your single file
     return ob_get_clean();
 }
-add_shortcode('words-count', 'cc_tool_shortcode');
+add_shortcode('words-count', 'tm_words_count_shortcode');
